7ddec6d4310683d79247070eb312a2ed469d3936
trangpt226 <82557338+trangpt226@users.noreply.github.com>
2024-05-16 12:01:55 +0700

    thay bai 24

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,138 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6BuiyGSHLCu":
+        Script1();
+        break;
+      case "5txWh1MTRrJ":
+        Script2();
+        break;
+      case "6RD0SmYVxSU":
+        Script3();
+        break;
+      case "5rLeJItf5Q5":
+        Script4();
+        break;
+      case "5vpGFYENa9y":
+        Script5();
+        break;
+      case "5keaBLxDcXK":
+        Script6();
+        break;
+      case "66IL4Rc5IJz":
+        Script7();
+        break;
+      case "5ymdUfB3VHX":
+        Script8();
+        break;
+      case "6f6RN0QKYFB":
+        Script9();
+        break;
+      case "6bCA3F27Zsc":
+        Script10();
+        break;
+      case "6PAkRmjD6qM":
+        Script11();
+        break;
+      case "6fAoqQYBira":
+        Script12();
+        break;
   }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+var e1 = player.GetVar('ô111');
+var e2 = e1.replace(/\s+/g, ''); 
+player.SetVar('ô111',e2);
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+var e3 = player.GetVar('ô222');
+var e4 = e3.replace(/\s+/g, ''); 
+player.SetVar('ô222',e4);
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+var e1 = player.GetVar('oo1');
+var e2 = e1.replace(/\s+/g, ''); 
+player.SetVar('oo1',e2);
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+var e3 = player.GetVar('oo2');
+var e4 = e1.replace(/\s+/g, ''); 
+player.SetVar('oo2',e4);
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+var e5 = player.GetVar('oo3');
+var e6 = e1.replace(/\s+/g, ''); 
+player.SetVar('oo3',e6);
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+var e7 = player.GetVar('oo4');
+var e8 = e1.replace(/\s+/g, ''); 
+player.SetVar('oo4',e8);
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+var e3 = player.GetVar('ovd5');
+var e4 = e3.replace(/\s+/g, ''); 
+player.SetVar('ovd5',e4);
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+var e3 = player.GetVar('ô2');
+var e4 = e3.replace(/\s+/g, ''); 
+player.SetVar('ô2',e4);
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+var e3 = player.GetVar('ô3');
+var e4 = e3.replace(/\s+/g, ''); 
+player.SetVar('ô3',e4);
+}
+
+function Script10()
+{
+  var player = GetPlayer();
+var e3 = player.GetVar('ô1');
+var e4 = e3.replace(/\s+/g, ''); 
+player.SetVar('ô1',e4);
+}
+
+function Script11()
+{
+  var player = GetPlayer();
+var e3 = player.GetVar('ô4');
+var e4 = e3.replace(/\s+/g, ''); 
+player.SetVar('ô4',e4);
+}
+
+function Script12()
+{
+  var player = GetPlayer();
+var e3 = player.GetVar('ô5');
+var e4 = e3.replace(/\s+/g, ''); 
+player.SetVar('ô5',e4);
 }
 
